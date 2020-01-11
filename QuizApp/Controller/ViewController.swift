e3248f703d84fbaba149a02fe1ce68240eba084b
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-11 21:42:31 +0900

    ViewControllerの実装内容変更

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -12,61 +12,78 @@ class ViewController: UIViewController {
     
     let allQuestion = QuestionBank()
     
+    //問題文を表示
+    @IBOutlet weak var questionLabel: UILabel!
+    //回答１を表示
+    @IBOutlet weak var answer1String: UIButton!
+    //回答2を表示
+    @IBOutlet weak var answer2String: UIButton!
+    
+    //現在の問題数
     var questionNumber = 0
-    var pickedAnswer = false
+    //選択された回答　回答1なら1、回答2なら2になる
+    var pickedAnswer = 0
+    //正解数をprintで表示
+    var correctAnswerNumber = 0
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        questionLabel.text = allQuestion.list[0].questionText
+        //初期に表示される画面の実装
+        questionLabel.text = allQuestion.list[0].questionText
+        answer1String.titleLabel?.text = allQuestion.list[0].answer1
+        answer2String.titleLabel?.text = allQuestion.list[0].answer2
     }
 
     
+
     
-    func answer(_ sender: Any) {
+    @IBAction func answer(_ sender: Any) {
         
         if (sender as AnyObject).tag == 1 {
-            pickedAnswer = true
-        
-        } else if (sender as AnyObject).tag == 2{
-         pickedAnswer = false
-        
+            pickedAnswer = 1
+        } else if (sender as AnyObject).tag == 2 {
+            pickedAnswer = 2
         }
         
-        //チェック　回答があっているのか
+        //問題に対する、回答が正しいのか
         check()
         
-        
         //次の問題を出題
-        nextQuestion()
+        
         
     }
     
+    //回答を確認する
+    func check(){
+        
+        let correctAnswer = allQuestion.list[0].correctCheck
+        
+        if correctAnswer == pickedAnswer {
+            print("正解")
+            correctAnswerNumber = correctAnswerNumber + 1
+        } else {
+            print("不正解")
+        }
+    }
     
-//    func check(){
-//        
-//        let correctAnswer = allQuestion.list[0].answer
-//        
-//        
-//        if correctAnswer == pickedAnswer {
-//            print("正解！")
-//        } else {
-//            print("間違い！")
-//        }
-//        
-//    }
-//    
-//    func nextQuestion(){
-//        if questionNumber <= 2 {
-//            questionNumber = questionNumber + 1
-//            questionLabel.text = allQuestion.list[questionNumber].questionText
-//        }else{
-//            print("終わり")
-//        }
-//    }
-//    
+    //次の質問へ進む
+    func nextQuestion(){
+        if questionNumber <= 1 {
+            questionNumber = questionNumber + 1
+            //問題文
+            questionLabel.text = allQuestion.list[questionNumber].questionText
+            //回答1
+            answer1String.titleLabel?.text = allQuestion.list[questionNumber].answer1
+            //回答2
+            answer2String.titleLabel?.text = allQuestion.list[questionNumber].answer2
+        } else {
+            print("質問終了")
+        }
+    }
+    
 
 }
 
