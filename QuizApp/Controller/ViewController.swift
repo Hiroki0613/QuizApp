dc16ae14857808680183b0d83760dc245408df25
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-11 19:39:57 +0900

    Storyboard実装、questionLabelに初期表示させる情報を実装

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let allQuestion = QuestionBank()
+    
+    var questionNumber = 0
+    var pickedAnswer = false
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        questionLabel.text = allQuestion.list[0].questionText
     }
 
+    
+    @IBAction func answer(_ sender: Any) {
+        
+    }
+    
 
 }
 
