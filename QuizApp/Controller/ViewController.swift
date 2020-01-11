ce639860e3695c021d19349d6ec857268727ffef
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-11 18:10:57 +0900

    Initial Commit

---
+++ QuizApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by 宏輝 on 11/01/2020.
+//  Copyright © 2020 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
